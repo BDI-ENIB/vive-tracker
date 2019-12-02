22001547b0c0d671c38db45b718695263d3c617b
Evan Roué <roue.evan1@gmail.com>
2019-12-02 11:39:55 +0100

    Add AGPL licence to the whole project
    See #11.

--- code/ViveBeacons/ViveBeacons.cydsn/main.h
+++ code/ViveBeacons/ViveBeacons.cydsn/main.h
@@ -1,13 +1,21 @@
-/* ========================================
+/* ============================================================================
  *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
+ *  Copyright (C) 2019 - Evan Roué
  *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
  *
- * ========================================
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ============================================================================
 */
 
 #if !defined(MAIN_H)
